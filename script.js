dc90c6fe9d290cd3f3929eced187750bf137bf22
JDFIREX <42634156+JDFIREX@users.noreply.github.com>
2020-09-10 06:52:07 +0200

    .

---
+++ script.js
@@ -0,0 +1,6 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamRmaXJleCIsImEiOiJja2JmZXBhY3QwdWw2MnNxZTlnY2N1ZDM4In0.Q34Lq3f1apoEpcn8teup1w';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
